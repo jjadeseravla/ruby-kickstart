e0e41e6edf262eda0255a85fa0b94fd035c39f98
jade <jadealvares@hotmail.co.uk>
2017-04-16 11:24:03 +0100

    Challenge_1

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,29 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  output = []
+  if return_odds
+    for char, index in string.split("").each_with_index
+      if index.odd?
+        output.push(char)
+      end
+    end
+  else
+    for char, index in string.split("").each_with_index
+      if index.even?
+        output.push(char)
+      end
+    end
+  end
+  output.join("")
 end
+
+#else
+  #return string.to_a.select.each_with_index { |i| i.even? }
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,20 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+
+  output = []
+  h = string.chars.each_index.select {|index| string[index] == "r" || string[index] == "R"}
+  #h.select {|index| index + 1}
+  #g.select {|index| index.to_i+1}.join
+
+  string_array = string.chars
+  h.each do |index|
+    output.push(string_array[index + 1])
+  end
+
+  return output.join("")
 end
